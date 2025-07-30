39a8c7925155cbe03a47fe27b9a5518456f65e70
Hamza Ahmed Shaikh <99156466+HamzaAhmed04@users.noreply.github.com>
2025-07-31 00:52:36 +0500

    Add files via upload

---
+++ JS Assisgnment 1/app.js
@@ -0,0 +1,23 @@
+// alert ("Hamza Ahmed ")
+// alert ("Shaikh")
+
+
+
+// var fName = ("Hamza Ahmed ")
+// var lName = ("Shaikh")
+
+// console.log (fName + lName)
+
+
+
+// var fName = ("Hamza Ahmed ")
+// var lName = ("Shaikh")
+
+// alert (fName + lName)
+
+
+
+var fName = ("Hamza Ahmed ")
+var lName = ("Shaikh")
+
+document.writeln (fName + lName)
