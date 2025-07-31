95b023679adc8cb3a6eef4b7639c7959ef80ec17
Hamza Ahmed Shaikh <99156466+HamzaAhmed04@users.noreply.github.com>
2025-08-01 01:17:14 +0500

    Update app.js

--- JS Assisgnment 1/app.js
+++ JS Assisgnment 1/app.js
@@ -17,7 +17,85 @@
 
 
 
-var fName = ("Hamza Ahmed ")
-var lName = ("Shaikh")
+// var fName = ("Hamza Ahmed ")
+// var lName = ("Shaikh")
 
-document.writeln (fName + lName)
+// document.writeln (fName + lName)
+
+
+// alert ("Welcome to JS Land  ")
+
+
+// var fName = (' <br>Hamza Ahmed </br>')
+// var lName = ('Shaikh')
+
+// document.writeln (fName + lName)
+
+
+// var num1 = +prompt('Enter a value 1');
+// var num2 = +prompt('Enter a value 2');
+
+// document.writeln (num1 + num2)
+
+// var username = ('Hamzario')
+// var myName = ('Hamza Ahmed Shaikh')
+// var Message = ("Hello World!")
+
+// alert (Message)
+
+
+// var fName = ("Jude Bellingham")
+
+// alert (fName)
+
+// var age =("22 years old")
+// alert(age)
+
+// var accupation =("international footballer")
+// alert(accupation)
+
+
+// var myPizza = ("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// alert (myPizza)
+
+
+// var eMail = ("mrhamzaahmedshaikh@gmail.com")
+// var note = ("My Email Address is ")
+// alert ( note + eMail)
+
+// var book = ('I am trying to learn from the A Smarter way to Javascript')
+// alert (book)
+
+// var ohYes = ("Yah! I can write HTML content through Javascript")
+
+// document.writeln (ohYes)
+
+// var theEnd = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// alert (theEnd)
+
+//#chapter3
+
+// var myAge = ("I am 18 years old");
+
+// alert (myAge)
+
+// var timeLine = ("you have visited this site 14 times");
+
+
+// alert (timeLine)
+
+// var birthYear = ("My birth year is 1947 <br> Data type of my declared variable is strings</br>");
+
+
+
+// document.writeln (birthYear)
+
+var visitorName = ("Jude Bellingham ordered ");
+
+var productTitle =  (" Jersey");
+var quantity = (11);
+var store = (" on XZY clothing store");
+
+document.writeln (visitorName + quantity + productTitle + store);
